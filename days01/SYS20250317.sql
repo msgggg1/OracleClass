8ebeb24706f17a75b6f78f4cc3f6850989941f9e
Minsun Kim <msggms1@gmail.com>
2025-03-17 22:49:05 +0900

    days01 복습

--- days01/SYS20250317.sql
+++ days01/SYS20250317.sql
@@ -1,8 +1,8 @@
 -- 모든 사용자 계정 정보를 조회하는 쿼리(SQL문)을/를 작성하세요 
 -- Query(DQL) 데이터 검색(조회) : SELECT
 SELECT * -- 조회하겠다.
-FROM all_users; -- CTRL + ENTER -- 암기
---FROM 대상(테이블 또는 뷰)
+FROM all_users;  -- 암기
+--FROM 대상(테이블 또는 뷰) -- CTRL + ENTER
 
 --PL/SQL 제어문 변수선언 등을 추가환 확장된 질의 언어
 -- SQL*Plus 
@@ -20,12 +20,21 @@ CREATE USER scott IDENTIFIED BY tiger;
 -- 상태: 실패 -테스트 실패: ORA-01045: user SCOTT lacks CREATE SESSION privilege; logon denied
 -- SYS -> SCOTT CREATE SESSION 권한 부여
 -- DCL : GRANT
+-- 신입사원 : 50명 + 신입사원역할(롤)
+--    신입 권한 50개 / 회수
+--          영업부롤 - 30개
+--          총무부롤 - 60개
+--          개발부롤 - 45개
+--            ㄴ 서주원 : 영업부롤 1개 부여/회수,  개발부롤 1개부여
+--            :  
 
+--  신입사원역할(롤) : 50개 권한
 -- 미리 정의된 롤 확인 : dba_roles 뷰
+
 SELECT *
 FROM dba_roles;
 
--- 미리 정의된 롤에 부여된 건환을 확인 : DBA_SYS_PRIVS뷰
+-- 미리 정의된 롤에 부여된 권환을 확인 : DBA_SYS_PRIVS뷰
 SELECT *
 FROM dba_sys_privs;
 --
