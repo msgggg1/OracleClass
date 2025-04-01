01afa245a1c83bff84a76070dd212539161ebd4a
Minsun Kim <msggms1@gmail.com>
2025-04-01 23:49:48 +0900

    days12 복습

---
+++ days12/SCOTT20250401_review.sql
@@ -0,0 +1,128 @@
+CREATE TABLE book(
+       b_id     VARCHAR2(10)    NOT NULL PRIMARY KEY   -- 책ID
+      ,title      VARCHAR2(100) NOT NULL  -- 책 제목
+      ,c_name  VARCHAR2(100)    NOT NULL     -- c 이름
+     -- ,  price  NUMBER(7) NOT NULL
+ );
+ 
+INSERT INTO book (b_id, title, c_name) VALUES ('a-1', '데이터베이스', '서울');
+INSERT INTO book (b_id, title, c_name) VALUES ('a-2', '데이터베이스', '경기');
+INSERT INTO book (b_id, title, c_name) VALUES ('b-1', '운영체제', '부산');
+INSERT INTO book (b_id, title, c_name) VALUES ('b-2', '운영체제', '인천');
+INSERT INTO book (b_id, title, c_name) VALUES ('c-1', '워드', '경기');
+INSERT INTO book (b_id, title, c_name) VALUES ('d-1', '엑셀', '대구');
+INSERT INTO book (b_id, title, c_name) VALUES ('e-1', '파워포인트', '부산');
+INSERT INTO book (b_id, title, c_name) VALUES ('f-1', '엑세스', '인천');
+INSERT INTO book (b_id, title, c_name) VALUES ('f-2', '엑세스', '서울');
+
+COMMIT;
+
+CREATE TABLE danga(
+      b_id  VARCHAR2(10)  NOT NULL  -- PK , FK
+      ,price  NUMBER(7) NOT NULL    -- 책 가격
+      
+      ,CONSTRAINT PK_danga_id PRIMARY KEY(b_id)
+      ,CONSTRAINT FK_danga_id FOREIGN KEY (b_id) --PK인 동시에 FK
+              REFERENCES book(b_id)
+              ON DELETE CASCADE
+);
+
+--(사진1) DANGA 테이블 - 변동되는 책값 기록 남겨야할때
+
+INSERT INTO danga (b_id, price) VALUES ('a-1', 300);
+INSERT INTO danga (b_id, price) VALUES ('a-2', 500);
+INSERT INTO danga (b_id, price) VALUES ('b-1', 450);
+INSERT INTO danga (b_id, price) VALUES ('b-2', 440);
+INSERT INTO danga (b_id, price) VALUES ('c-1', 320);
+INSERT INTO danga (b_id, price) VALUES ('d-1', 321);
+INSERT INTO danga (b_id, price) VALUES ('e-1', 250);
+INSERT INTO danga (b_id, price) VALUES ('f-1', 510);
+INSERT INTO danga (b_id, price) VALUES ('f-2', 400);
+
+COMMIT;
+
+CREATE TABLE au_book(
+       id   number(5)  NOT NULL PRIMARY KEY
+      ,b_id VARCHAR2(10)  NOT NULL  CONSTRAINT FK_AUBOOK_BID
+            REFERENCES book(b_id) --ON DELETE CASCADE, 책 여러권일 수 있음
+      ,name VARCHAR2(20)  NOT NULL
+);
+INSERT INTO au_book (id, b_id, name) VALUES (1, 'a-1', '저팔개');
+INSERT INTO au_book (id, b_id, name) VALUES (2, 'b-1', '손오공');
+INSERT INTO au_book (id, b_id, name) VALUES (3, 'a-1', '사오정');
+INSERT INTO au_book (id, b_id, name) VALUES (4, 'b-1', '김유신');
+INSERT INTO au_book (id, b_id, name) VALUES (5, 'c-1', '유관순');
+INSERT INTO au_book (id, b_id, name) VALUES (6, 'd-1', '김하늘');
+INSERT INTO au_book (id, b_id, name) VALUES (7, 'a-1', '심심해'); -- 3명이 저자
+INSERT INTO au_book (id, b_id, name) VALUES (8, 'd-1', '허첨');
+INSERT INTO au_book (id, b_id, name) VALUES (9, 'e-1', '이한나');
+INSERT INTO au_book (id, b_id, name) VALUES (10, 'f-1', '정말자');
+INSERT INTO au_book (id, b_id, name) VALUES (11, 'f-2', '이영애');
+
+COMMIT;
+
+CREATE TABLE gogaek(
+      g_id       NUMBER(5) NOT NULL PRIMARY KEY 
+      ,g_name   VARCHAR2(20) NOT NULL
+      ,g_tel      VARCHAR2(20)
+);
+ 
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (1, '우리서점', '111-1111');
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (2, '도시서점', '111-1111');
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (3, '지구서점', '333-3333');
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (4, '서울서점', '444-4444');
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (5, '수도서점', '555-5555');
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (6, '강남서점', '666-6666');
+INSERT INTO gogaek (g_id, g_name, g_tel) VALUES (7, '강북서점', '777-7777');
+
+COMMIT;
+
+CREATE TABLE panmai(
+       id         NUMBER(5) NOT NULL PRIMARY KEY
+      ,g_id       NUMBER(5) NOT NULL CONSTRAINT FK_PANMAI_GID
+                     REFERENCES gogaek(g_id) ON DELETE CASCADE
+      ,b_id       VARCHAR2(10)  NOT NULL CONSTRAINT FK_PANMAI_BID
+                     REFERENCES book(b_id) ON DELETE CASCADE
+      ,p_date     DATE DEFAULT SYSDATE
+      ,p_su       NUMBER(5)  NOT NULL
+);
+
+
+
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (1, 1, 'a-1', '2000-10-10', 10);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (2, 2, 'a-1', '2000-03-04', 20);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (3, 1, 'b-1', DEFAULT, 13);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (4, 4, 'c-1', '2000-07-07', 5);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (5, 4, 'd-1', DEFAULT, 31);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (6, 6, 'f-1', DEFAULT, 21);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (7, 7, 'a-1', DEFAULT, 26);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (8, 6, 'a-1', DEFAULT, 17);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (9, 6, 'b-1', DEFAULT, 5);
+INSERT INTO panmai (id, g_id, b_id, p_date, p_su) VALUES (10, 7, 'a-2', '2000-10-10', 15);
+
+COMMIT;
+
+CREATE OR REPLACE VIEW uvpan
+    -- 칼럼명 그대로 사용
+AS 
+SELECT b.b_id, title, price, g.g_id, g_name, p_date, p_su* price amt
+FROM book b JOIN danga d ON b.b_id = d.b_id
+            JOIN panmai p ON b.b_id = p.b_id
+            JOIN gogaek g ON p.g_id = g.g_id;
+            
+            select *
+from uvpan -- 뷰
+where b_id = 'a-1'
+ORDER by p_date; 
+
+CREATE OR REPLACE VIEW uvgogaek
+AS
+select to_char( p_date, 'YYYY' )p_year
+        , to_char( p_date, 'MM')p_month
+        , g.g_id, g_name,Sum( price*p_su) 판매금액합
+FROm panmai p JOIN gogaek g on p.g_id = g.g_id
+              JOIN danga d on p.b_id = d.b_id
+GROUP by to_char( p_date, 'YYYY' )
+        , to_char( p_date, 'MM')
+        , g.g_id, g_name
+ORDER BY p_year, p_month;
